bdc5de9646d61fb1dbdb7418597f9752854c8ab4
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-03-10 08:39:35 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    char()
+    remainder()
+    greeting()
+    length()
+
+//Function that prints out a string
+}
+fun char(){
+    var name = "codeHive"
+    var n = name[4].toString()+ name[6]+ name[7]
+    println(n)
+}
+//Function that returns remainder
+fun remainder(){
+    var y = 9/3
+    println(y)
+}
+fun greeting() {
+    var b = ("Hi my name is p" + " I am z years old")
+    println(b)
+}
+//Function in two parameters
+fun length() {
+    var y = "I am tall"
+    println(y.length)
+}
